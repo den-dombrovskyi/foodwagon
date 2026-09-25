a13718bc153899b502b89ffd7ab3ae35a75c3346
Denis Dombrovskyi <ddombrovskyi25@gmail.com>
2026-09-25 18:21:06 +0300

    fix: update selectors in main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
+const subscribeForm =
+  document.querySelector(".subscribe__form") ||
+  document.querySelector(".footer__form") ||
+  document.getElementById("subscribe-form");
+const subscribeEmail =
+  document.querySelector(".subscribe__input") ||
+  document.querySelector(".footer__input") ||
+  document.getElementById("subscribe-email");
 
-const subscribeForm = document.getElementById('subscribe-form');
-const subscribeEmail = document.getElementById('subscribe-email');
+subscribeForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-subscribeForm.addEventListener('submit', function(event) {
- 
-  event.preventDefault(); 
-  
-  const userEmail = subscribeEmail.value; 
-  
- 
+  const userEmail = subscribeEmail.value;
+
   alert(`Дякую за підписку! Ми відправили найкращі пропозиції на ${userEmail}`);
-  
-  subscribeEmail.value = '';
+
+  subscribeEmail.value = "";
 });
 
+const cityLinks = document.querySelectorAll(
+  ".footer__cities-grid .footer__link",
+);
 
-const cityLinks = document.querySelectorAll('.footer__cities-grid .footer__link');
+cityLinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
 
-cityLinks.forEach(function(link) {
-  link.addEventListener('click', function(event) {
-    event.preventDefault(); 
-    
     const cityName = link.textContent;
-    
+
     alert(`Шукаємо найкращі пропозиції та ресторани у місті: ${cityName}...`);
   });
 });
